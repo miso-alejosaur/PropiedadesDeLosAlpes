5a24e92a7bc820f9617d1995cd43fab653563f3e
rcaicedom <r.caicedom@uniandes.edu.co>
2024-03-08 05:04:22 +0000

    enable mutacion

--- src/bff_investigacion/api/v1/esquemas.py
+++ src/bff_investigacion/api/v1/esquemas.py
@@ -3,6 +3,8 @@ import strawberry
 import uuid
 import requests
 import os
+from pulsar.schema import *
+from src.bff_investigacion import utils
 
 COREANALITICA_ADDRESS = os.getenv("COREANALITICA_ADDRESS", default="localhost")
 ID="Colombia"
@@ -25,3 +27,44 @@ class Metrica:
     pais: str
     valor_compra: float
     valor_arrendamiento: float
+
+@strawberry.type
+class ConfiabilidadRespuesta:
+    mensaje: str
+    codigo: int
+
+class Mensaje(Record):
+    id = String(default=str(uuid.uuid4()))
+    time = Long()
+    ingestion = Long(default=utils.time_millis())
+    specversion = String()
+    type = String()
+    datacontenttype = String()
+    service_name = String()
+
+class EventoIntegracion(Mensaje):
+    ...
+    
+class ComandoIntegracion(Mensaje):
+    ...
+
+class ActualizarIndiceConfiabilidadPayload(ComandoIntegracion):
+    id_propiedad = String()
+    indice_confiabilidad = Float()
+
+class ComandoActualizarIndiceConfiabilidad(ComandoIntegracion):
+    data = ActualizarIndiceConfiabilidadPayload()
+
+class PropiedadDisponiblePayload(Record):
+    id_propiedad = String()
+    valor_compra = Float()
+    valor_arrendamiento = Float()
+    divisa = String()
+    pais = String()
+    indice_confiabilidad = Float()
+
+class EventoPropiedadDisponible(EventoIntegracion):
+    data = PropiedadDisponiblePayload()
+
+class EventoIndiceActualizado(EventoIntegracion):
+    data = PropiedadDisponiblePayload()
--- src/bff_investigacion/despachadores.py
+++ src/bff_investigacion/despachadores.py
@@ -2,6 +2,7 @@ import pulsar
 from pulsar.schema import *
 
 from . import utils
+from src.bff_investigacion.api.v1.esquemas import ComandoActualizarIndiceConfiabilidad
 
 class Despachador:
     def __init__(self):
@@ -10,6 +11,6 @@ class Despachador:
     async def publicar_mensaje(self, mensaje, topico, schema):
 
         cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
-        publicador = cliente.create_producer("", schema=AvroSchema())
+        publicador = cliente.create_producer(topico, schema=AvroSchema(ComandoActualizarIndiceConfiabilidad))
         publicador.send(mensaje)
         cliente.close()
